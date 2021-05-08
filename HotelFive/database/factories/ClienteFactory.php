83ae405fbd91679c2faad60cb13f54b14cb1f5c8
AgustinGM09 <72084815+AgustinGM09@users.noreply.github.com>
2021-05-08 18:52:56 -0300

    Cliente
    modelo, migración y factory creados.

---
+++ HotelFive/database/factories/ClienteFactory.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Cliente;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ClienteFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Cliente::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'dni' => $this->faker->unique()->numberBetween(10000000, 99999999),
+            'pasaporte' => $this->faker->unique()->numberBetween(10000000, 99999999),
+            'nombre' => $this->faker->name,
+            'apellido' => $this->faker->lastname(),
+            'email' => $this->faker->unique()->safeEmail(),
+            'telefono' => $this->faker->numberBetween(1000000000, 9999999999),
+        ];
+    }
+}
